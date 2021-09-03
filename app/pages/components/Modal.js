a0fea4a9a060926b40999d696920c1a419da9f4c
Francesco Palomba <palomba.fra@gmail.com>
2021-09-03 12:20:44 +0200

    hide dot on new task modal

--- app/pages/components/Modal.js
+++ app/pages/components/Modal.js
@@ -4,7 +4,10 @@ export const Modal = ({title, show, onClose, children, status}) => {
             <div className="modal-content" onClick={e => e.stopPropagation()}>
                 <div className="modal-header">
                     <span className="mt-0">
-                        <span className={"dot" + (status ? " high-prio" : "")}/>
+                        {
+                            status !== undefined &&
+                                <span className={"dot" + (status ? " high-prio" : "")}/>
+                        }
                         {title}</span>
                     <button className="light icon" onClick={onClose}>
                         <img className="rotate-45" alt="Close icon" src="icons/icon-close.svg" height={20} width={20}/>
